de2f83713fe9df62fc51b12247e878c58b3fb89e
Kaitlyn <e.kaitlyn.oof@gmail.com>
2024-07-02 21:14:37 -0400

    yes

--- hw/e108.py
+++ hw/e108.py
@@ -1,8 +1,23 @@
 num = input()
 nums = []
+negative = []
+zero = []
+positive = []
+sort = []
 while num != "":
     nums.append(int(num))
     num = input()
-nums.sort()
+
 for i in range(len(nums)):
-    print(nums[i])
+    if nums[i]<0:
+        negative.append(nums[i])
+    elif nums[i]==0:
+        zero.append(nums[i])
+    else:
+        positive.append(nums[i])
+
+sort.append(negative)
+sort.append(zero)
+sort.append(positive)
+for i in range(len(sort)):
+    print(sort[i])
