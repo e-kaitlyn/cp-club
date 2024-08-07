5eac5e4ecf715c777b346a52b3b8b110651473a3
Kaitlyn <e.kaitlyn.oof@gmail.com>
2024-08-06 21:27:06 -0400

    yes

--- hw/e138.py
+++ hw/e138.py
@@ -1,17 +1,24 @@
 import random
+
 def bingo():
-    b,i,n,g,o=[],[],[],[],[]
+    bingo = {}
     ranges = [1,16,31,46,61,15,30,45,60,75]
     nums = [random.randint(1,15)]
-    for i in range(74):
-        x = (i+1)//15
+    for i in range(24):
+        x = (i+1)//5
         nums.append(random.randint(ranges[x],ranges[x+5]))
-    b = nums[:15]
-    i = nums[15:30]
-    n = nums[30:45]
-    g = nums[45:60]
-    o = nums[60:]
-    print("B I  N  G  O")
-    for x in range(15):
-        print(b[x],i[x],n[x],g[x],o[x],"\n")
-bingo()
+    bingo['B'] = nums[:5]
+    bingo['I'] = nums[5:10]
+    bingo['N'] = nums[10:15]
+    bingo['G'] = nums[15:20]
+    bingo['O'] = nums[20:]
+    return bingo
+card = bingo()
+keys = ['B','I','N','G','O']
+for k in card:
+    print(f"   {k} ",end="")
+print()
+for i in range(5):
+    for letter in keys:
+        print("%4d " % card[letter][i], end="")
+    print()
